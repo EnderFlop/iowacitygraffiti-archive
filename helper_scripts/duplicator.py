d56e28a22ea5b5256ced7401dda2939c891b5495
enderflop <agbarloon@uiowa.edu>
2023-08-01 16:49:39 -0500

    precommit linn st

---
+++ helper_scripts/duplicator.py
@@ -0,0 +1,2 @@
+#takes in img, thumbnail, json, and duplicates them into folder with new (inputted) artist.
+#allows for post-archive duplication, like grabbing all the SNOTNOSE photos from PUFF and OK's pages
